c41cddc0cd7d4f00c50268e88589370bf0aca077
AntoMei <antonio.meirosu444@gmail.com>
2022-12-21 09:28:29 +0100

    Finalizada la parte de panel de control

--- symfony-tienda/src/Controller/Admin/DashboardController.php
+++ symfony-tienda/src/Controller/Admin/DashboardController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Team;
+use App\Controller\Admin\TeamCrudController;
+use App\Entity\Product;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,7 +17,9 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+    $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+
+    return $this->redirect($adminUrlGenerator->setController(TeamCrudController::class)->generateUrl());
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
--- symfony-tienda/src/Controller/PageController.php
+++ symfony-tienda/src/Controller/PageController.php
@@ -2,22 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Team;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\ProductsService;
 
 class PageController extends AbstractController
 {
-    #[Route('/', name: 'index')]
-    public function index(): Response
+    public function teamTemplate(ManagerRegistry $doctrine): Response
     {
-        return $this->render('page/index.html.twig', []);
+        $repository = $doctrine->getRepository(Team::class);
+        $team = $repository->findAll();
+        return $this->render('partials/_team.html.twig', 
+            compact('team')
+        );
+    }
+
+    #[Route('/', name: 'index')]
+    public function index(ProductsService $productsService): Response
+    {
+        $products = $productsService->getProducts();
+        return $this->render('page/index.html.twig', compact('products'));
     }
 
     #[Route('/about', name: 'about')]
-    public function about(): Response
+    public function about(ManagerRegistry $doctrine): Response
     {
-        return $this->render('page/about.html.twig', []);
+        return $this->render('page/about.html.twig');
     }
 
     #[Route('/service', name: 'service')]
